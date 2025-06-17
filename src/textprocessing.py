3d4a83499130f1520e64dd50db977328ef8b0679
David Woodard <drwoodard72@domain.com>
2025-06-17 13:13:23 -0500

    pushing unfinished code to repo for split_nodes_image

--- src/textprocessing.py
+++ src/textprocessing.py
@@ -35,3 +35,23 @@ def extract_markdown_images(text):
 
 def extract_markdown_links(text):
     return re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)",text)
+
+def split_nodes_image(oldnodes:list[TextNode]):
+    newNodes = []
+    for oldnode in oldnodes:
+        original_text = oldnode.text
+        matches = extract_markdown_images(original_text)
+        if len(matches == 0):
+            newNodes.append(oldnode)
+            continue
+        while matches != None and len(matches) > 0:
+            sections = original_text.split(f"![{matches[0][0]}]({matches[0][1]})", 1)
+            newNodes.append(TextNode(sections[0],TextType.TEXT))
+            newNodes.append(TextNode(matches[0][0],TextType.IMAGE,matches[0][1]))
+            if len(matches == 1) and len(sections == 2):
+                newNodes.append(TextNode(sections[1],TextType.TEXT))
+            elif len(matches > 1) and len(sections == 2):
+                original_text = sections[1]
+                matches = matches[1:]
+            else:
+                # todo: sanity check? is this an error state... 
