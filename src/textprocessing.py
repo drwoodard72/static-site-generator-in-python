51bd92d7bbc1722cea4cc895e24883e226d6c76a
David Woodard <121324176+drwoodard72@users.noreply.github.com>
2025-09-09 14:55:48 -0500

    cleanup indentations

--- src/textprocessing.py
+++ src/textprocessing.py
@@ -18,11 +18,11 @@ def split_nodes_delimiter(old_nodes:list[TextNode], delimiter:str, text_type:Tex
     
     for oldNode in old_nodes:
        if oldNode.text_type != TextType.TEXT:
-           newNodes.append(oldNode)
-           continue
+            newNodes.append(oldNode)
+            continue
        oldParts = oldNode.text.split(delimiter)
        if len(oldParts) != 3:
-           raise Exception("Invalid markdown text")
+            raise Exception("Invalid markdown text")
        oldParts[0] = TextNode(oldParts[0],TextType.TEXT)
        oldParts[1] = TextNode(oldParts[1],newTextType)
        oldParts[2] = TextNode(oldParts[2],TextType.TEXT)
