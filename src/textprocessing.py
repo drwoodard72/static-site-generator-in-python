4000eaf914899ce8dfba7a192a72c3adc89a7995
David Woodard <121324176+drwoodard72@users.noreply.github.com>
2025-10-31 19:01:30 -0500

    add markdown_to_html_node()

--- src/textprocessing.py
+++ src/textprocessing.py
@@ -1,6 +1,7 @@
 from textnode import *
 import re
 from enum import Enum
+from htmlnode import *
 
 BlockType = Enum('BlockType', ['paragraph','heading','code','quote','unordered_list','ordered_list'])
 
@@ -155,3 +156,17 @@ def block_to_block_type(markdown):
 
     # If none of the above conditions are met, the block is a normal paragraph.
     return BlockType.paragraph
+
+def markdown_to_html_node(markdown):
+    parentHTMLNode = HTMLNode("div")
+
+# TODO:
+# Split the markdown into blocks (you already have a function for this)
+# Loop over each block:
+# Determine the type of block (you already have a function for this)
+# Based on the type of block, create a new HTMLNode with the proper data
+# Assign the proper child HTMLNode objects to the block node. I created a shared text_to_children(text) function that works for all block types. It takes a string of text and returns a list of HTMLNodes that represent the inline markdown using previously created functions (think TextNode -> HTMLNode).
+# The "code" block is a bit of a special case: it should not do any inline markdown parsing of its children. I didn't use my text_to_children function for this block type, I manually made a TextNode and used text_node_to_html_node.
+# Make all the block nodes children under a single parent HTML node (which should just be a div) and return it.
+
+    return parentHTMLNode
